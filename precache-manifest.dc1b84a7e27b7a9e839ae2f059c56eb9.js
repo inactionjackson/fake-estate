08deab007bd30d1641d5cfa35b57ec4181893e7b
Andrew Jackson <inactionjackson@gmail.com>
2019-09-18 00:51:08 -0400

    Updates

---
+++ precache-manifest.dc1b84a7e27b7a9e839ae2f059c56eb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "309a70c3ad34fb33505e6a1f6ab40578",
+    "url": "/fake-estate/static/media/kevin-young-7002-unsplash.309a70c3.jpg"
+  },
+  {
+    "revision": "a0f48696e25c8d56ad75",
+    "url": "/fake-estate/static/js/runtime~main.640898d5.js"
+  },
+  {
+    "revision": "25db8eef43dee09ce191",
+    "url": "/fake-estate/static/js/main.67cd5790.chunk.js"
+  },
+  {
+    "revision": "d8a4462656ffcfda2078",
+    "url": "/fake-estate/static/js/2.f4fc1e5b.chunk.js"
+  },
+  {
+    "revision": "25db8eef43dee09ce191",
+    "url": "/fake-estate/static/css/main.b9b3f5d9.chunk.css"
+  },
+  {
+    "revision": "f23a39a718a272aa02795f01143808b4",
+    "url": "/fake-estate/index.html"
+  }
+];
